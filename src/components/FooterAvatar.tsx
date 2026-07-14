fe87a03fc18a8c45a7fbd8ffdc1759f0083caebc
roshanimmanuel792 <roshanimmanuel10@gmail.com>
2026-07-14 22:21:38 +0530

    reverted back

--- src/components/FooterAvatar.tsx
+++ src/components/FooterAvatar.tsx
@@ -3,14 +3,7 @@
 import React, { useState, useEffect } from 'react';
 import styles from './FooterAvatar.module.css';
 
-const MESSAGES = [
-  "Hello there! You wanna know about Roshan?",
-  "I know a guy who knows a guy and he knows Roshan"
-];
-
 export default function FooterAvatar() {
-  const [displayedText, setDisplayedText] = useState('');
-  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
   const [eyesOpen, setEyesOpen] = useState(true);
 
   // Blinking animation
@@ -46,45 +39,14 @@ export default function FooterAvatar() {
     };
   }, []);
 
-  // Dialogue typing animation
-  useEffect(() => {
-    const currentMessage = MESSAGES[currentMessageIndex];
-    let charIndex = 0;
-
-    const typingInterval = setInterval(() => {
-      if (charIndex <= currentMessage.length) {
-        setDisplayedText(currentMessage.substring(0, charIndex));
-        charIndex++;
-      } else {
-        clearInterval(typingInterval);
-      }
-    }, 50);
-
-    const messageTimer = setTimeout(() => {
-      setCurrentMessageIndex((prev) => (prev + 1) % MESSAGES.length);
-      setDisplayedText('');
-    }, 6000);
-
-    return () => {
-      clearInterval(typingInterval);
-      clearTimeout(messageTimer);
-    };
-  }, [currentMessageIndex]);
-
   return (
     <div className={styles.pikaContainer}>
       <div className={styles.pikaImage}>
         <img
           src={eyesOpen ? "/character_eyes_open.png" : "/character_eyes_closed.png"}
           alt="Chatbot Avatar"
-          width={120}
-          height={120}
           className={styles.pikaImg}
         />
-      </div>
-      <div className={styles.dialogBox}>
-        <div className={styles.dialogPointer}></div>
-        <div className={styles.dialogText}>{displayedText}</div>
       </div>
     </div>
   );
